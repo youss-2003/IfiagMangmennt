7cb9bcac0fc2e73ec7709414c681bc9395f235b0
youss-2003 <youssef.ratbi222@gmail.com>
2025-05-29 16:49:02 +0100

    last commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import LoginScreen from "./(auth)/login";
 
 export default function Index() {
   return (
-    <View >
- 
-      <Text className="text-red-600">Edit app/index.tsx to edit this screen. <Link href={'/(auth)/register/step1'}>go here</Link></Text>
-    </View>
+    <LoginScreen/>
   );
 }
